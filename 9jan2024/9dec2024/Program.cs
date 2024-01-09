b61a71fd02d2180e47f27e3741588143a81a5a05
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-01-09 16:43:50 +0400

    Add files via upload

---
+++ 9jan2024/9dec2024/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace _9dec2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Domain("mulayimen@code.edu.az"));
+
+            Console.WriteLine(OnlyLetters("salam61"));
+
+            Console.WriteLine(Capitalize("sAlaM nEcEsEn"));
+
+            string[] fullnames = { "Jonas Bentley", "George Miller", "Lilith Appleton" };
+
+            string[] firstNames = Names(fullnames);
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                Console.WriteLine(firstNames[i]);
+            }
+
+            DateTime birthDate = new DateTime(2005, 5, 11); 
+            int age = MyAge(birthDate);
+            Console.WriteLine($"Yaşınız : {age}");
+
+        }
+        #region Verilmiş email dəyərinin domain hissəni qaytaran metod
+        static string Domain(string email)
+        {
+            return email.Substring(email.IndexOf('@') + 1);
+        }
+
+        #endregion
+
+        #region Verilmiş yazının yalnız hərflərdən ibarət olub olmadığını tapan metod
+        static bool OnlyLetters(string str)
+        {
+            return str.All(char.IsLetter);  
+        }
+
+        #endregion
+
+        #region Verilmiş yazının ilk hərfini böyük qalanlarını kiçik edib qaytaran metod
+        static string Capitalize(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            char firstChar = char.ToUpper(str[0]);
+            string restOfTheString = str.Substring(1).ToLower();
+
+            return firstChar + restOfTheString;
+
+        }
+
+        #endregion
+
+        #region Verilmiş fullnamelər siyahısından name-lər siyahısı düzəldən metod(ad soyadlar siyahısındaki adları kəsib yeni bir arraye doldurub qaytarmalıdır)
+        static string[] Names(string[] arr)
+        {
+            string[] names = new string[arr.Length]; 
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int spaceIndex = arr[i].IndexOf(' '); 
+                if (spaceIndex != -1) 
+                {
+                    names[i] = arr[i].Substring(0, spaceIndex); 
+                }
+                else
+                {
+                    names[i] = arr[i]; 
+                }
+            }
+            return names; 
+        }
+
+        #endregion
+
+        #region Doğum tarixinizi daxil etdiyinizdə sizə yaşınızı qaytaran metod
+        static int MyAge(DateTime birthdate)
+        {
+            DateTime currentDate = DateTime.Now; 
+
+            int age = currentDate.Year - birthdate.Year; 
+
+            if (birthdate.Date > currentDate.AddYears(-age))
+            {
+                age--; 
+            }
+
+            return age; 
+        }
+        #endregion
+
+    }
+
+}
---
+++ 9jan2024/tekrar_methodlar/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace tekrar_methodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = { 127, 3, 34, 52};
+            int[] newNumArr = new int[11];
+
+            ////numArr.CopyTo(newNumArr, );
+            //Array.Copy(numArr, 0, newNumArr, 5, 4);
+            ////Array.Clear(newNumArr, 0, 10);
+
+
+            Array.Sort(numArr);
+            Array.Reverse(numArr);
+            Array.Resize(ref numArr, 2);
+
+
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                Console.WriteLine(numArr[i]);
+            }
+            int[] numArr2 = { 127, 3, 34, 52,119 };
+
+            Console.WriteLine(Array.IndexOf(numArr2, 3));
+            Console.WriteLine(Array.LastIndexOf(numArr2, 3));
+
+            string text = " Salam doslar !   ";
+
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(text.Trim());
+            Console.WriteLine(text.Replace(' ', '-'));
+            Console.WriteLine(text.Replace('H', 'A'));
+            Console.WriteLine(text.Replace("doslar", "PB302"));
+            Console.WriteLine(text);
+            Console.WriteLine(text.Contains("lar "));
+            Console.WriteLine(text.Trim().ToUpper().StartsWith("SA"));
+            Console.WriteLine(text.EndsWith("r !   "));
+
+            string str = "Bugun  ucun derslerini  oxumusan  ?";
+
+            Console.WriteLine(str.IndexOf("ucun"));
+            Console.WriteLine(str.IndexOf('u'));
+            Console.WriteLine(str.LastIndexOf('u'));
+
+            var newStr = str.Substring(0, 4);
+            Console.WriteLine(newStr);
+
+            var arr = str.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            var newStr2 = String.Join('-', arr);
+            Console.WriteLine(newStr2);
+
+            char letter = 'a';
+
+            Console.WriteLine(char.ToUpper('d'));
+            Console.WriteLine(char.IsDigit('d'));
+            Console.WriteLine(char.IsUpper('o'));
+            Console.WriteLine(char.IsLetter('B'));
+            Console.WriteLine(char.IsLetterOrDigit('a'));
+        }
+    }
+}
